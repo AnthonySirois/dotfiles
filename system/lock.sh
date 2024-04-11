ea19d4f825f42bacda4ca4d92fada6fca2ca57b5
Anthony Sirois <anthonysirois999@gmail.com>
2024-04-11 18:49:12 -0400

    improve lock

--- system/lock.sh
+++ system/lock.sh
@@ -1,35 +1,52 @@
 #!/bin/sh
 
-BLANK='#12121f'
-CLEAR='#12121f'
-DEFAULT='#729ce9'
-TEXT='#d4d7ff'
-WRONG='#da587d'
-VERIFYING='#7fdca9'
+SCREEN_WIDTH=2560 
+SCREEN_HEIGHT=1440
+
+DEFAULT='#cba6f7'
+TEXT='#cdd6f4'
+WRONG='#f38ba8'
+VERIFYING='#fab387'
+
+# Pause notifications while the screen is locked
+# The -n parameter in i3lock is necessary
+dunstctl set-paused true
 
 i3lock \
-  --insidever-color=$CLEAR \
+  -n \
+  --verif-text="Verifying" \
+  --wrong-text="Incorrect password" \
+  --noinput-text="No input" \
+  \
   --ringver-color=$VERIFYING \
-  \
-  --insidewrong-color=$CLEAR \
   --ringwrong-color=$WRONG \
+  --keyhl-color=$DEFAULT \
+  --bshl-color=$DEFAULT \
   \
-  --inside-color=$BLANK \
-  --ring-color=$DEFAULT \
-  --line-color=$BLANK \
-  --separator-color=$DEFAULT \
-  \
-  --verif-color=$TEXT \
-  --wrong-color=$TEXT \
+  --verif-color=$VERIFYING \
+  --wrong-color=$WRONG \
+  --modif-color=$TEXT \
   --time-color=$TEXT \
   --date-color=$TEXT \
   --layout-color=$TEXT \
-  --keyhl-color=$WRONG \
-  --bshl-color=$WRONG \
   \
   --screen 1 \
-  --blur 7 \
   --clock \
+  --blur 7 \
   --indicator \
   --time-str="%H:%M:%S" \
   --date-str="%d-%m-%Y" \
+  --ignore-empty-password \
+  --bar-indicator \
+  --bar-direction 1 \
+  --bar-step 192 \
+  --bar-max-height 576 \
+  --bar-base-width 5 \
+  --bar-color=$DEFAULT \
+  --bar-periodic-step 72 \
+  --bar-pos h \
+  --bar-count 278 \
+  --bar-total-width $SCREEN_WIDTH+80 \
+# For some unknown reason, the screen width does not cover the whole screen
+
+dunstctl set-paused false
